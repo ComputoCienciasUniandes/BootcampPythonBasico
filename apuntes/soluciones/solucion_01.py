0c7a039a4ca6474e602d122d64c2ff5cadaa3f46
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2020-07-17 19:23:52 -0500

    enunciado

--- apuntes/soluciones/solucion_01.py
+++ apuntes/soluciones/solucion_01.py
@@ -1,3 +1,16 @@
+#Escriba un programa de python que:
+
+#1. Lea el archivo "texto.txt" usando readlines()
+
+#2. Cuente el número de veces que aparece en el texto cada una de las letras del alfabeto inglés (abcdefghijklmnopqrstuvwxyz). Solamente tenga en cuenta las letras que se encuentran en minúscula en el texto.
+
+#3. Escribe el resutado del conteo en el archivo "conteo.csv". El archivo debe contener dos columnas. La primera es la letra del alfabeto y la segunda es el número de veces que se encuentra esa letra en el texto. Si la letra no se encuentra debe escribir cero (0).
+
+#Solamente son permitidas las operaciones en python que aparezcan en los videos de la playlist del curso. El programa debe funcionar correctamente si el contenido de "texto.txt" cambia (suponiendo siempre que el archivo no está vacío). 
+
+#También puede utilizar que un diccionario vacío se crea como "a={}" y una lista vacía se crea como "b=[]".
+
+
 # leo el texto
 f = open('texto.txt', 'r')
 lineas = f.readlines()
